9062ee5ca9185dbbe738f99b3253f9e9c4dfb213
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2022-06-06 00:17:39 +0600

    bdb 1st section done

--- src/pages/BanggobondhuConrenr.jsx
+++ src/pages/BanggobondhuConrenr.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/BanggobondhuCorner/corner.css";
+import mujib from "../img/bonggobondhu/bonggobondhu.png";
 
 const BanggobondhuConrenr = () => {
     return (
@@ -7,8 +8,26 @@ const BanggobondhuConrenr = () => {
             <div className="section">
                 <div className="sectionOne">
                     <div className="row w-100 h-100 justify-content-center align-items-center">
-                        <div className="col-md-6 h-100 bg-danger"></div>
-                        <div className="col-md-6 h-100 bg-info"></div>
+                        <div className="col-md-6 h-100 d-flex flex-column justify-content-center align-items-center">
+                            <div className="heading">
+                                <div className="firstHead d-flex flex-column ">
+                                    <span>Sheikh</span>
+                                    <span>Mujibur</span>
+                                    <span>Rahman</span>
+                                </div>
+                                <div
+                                    data-text="Bangabandhu"
+                                    className="secondHead"
+                                >
+                                    Bangabandhu
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 h-100 position-relative">
+                            <div className="imageDiv">
+                                {/* <img src={mujib} alt="" /> */}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
